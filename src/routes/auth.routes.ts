b9bb592e133d3497ffdf1e9d4195234f4b200fb6
RodrigoHernandez26 <rodrigohsouza26@gmail.com>
2022-06-06 23:43:47 -0300

    add authuser and logout

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,6 +1,9 @@
 const router = require('express').Router();
-import { login } from "../controllers/auth.controller";
+import { authUser, login, logout } from "../controllers/auth.controller";
+import { verifyToken } from "../middlewares/auth.middleware";
 
 router.post('/login', login)
+router.post('/auth', verifyToken, authUser)
+router.post('/logout', logout)
 
 module.exports = router;
